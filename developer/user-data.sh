8d1599fd105c8e38e4f1c181a9e666072f51d174
Julian Andrej <andrej1@llnl.gov>
2022-08-24 10:46:08 -0700

    remove port

--- developer/user-data.sh
+++ developer/user-data.sh
@@ -3,4 +3,4 @@ yum update -y
 amazon-linux-extras install docker
 systemctl start docker
 systemctl enable docker
-docker run -p 3000:3000 -p 8000:8000 -p 8080:8080 -p 9090:9090 -p 19916:19916 ghcr.io/jandrej/mfem-developer
+docker run -p 3000:3000 -p 8000:8000 -p 8080:8080 -p 9090:9090 ghcr.io/jandrej/mfem-developer
